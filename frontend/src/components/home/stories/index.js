b74cd45d3659c811a5b114811173fd839e936525
AlonsoErnesto <ernesto134alonso123@gmail.com>
2022-10-07 21:04:54 -0500

    media_query_home

--- frontend/src/components/home/stories/index.js
+++ frontend/src/components/home/stories/index.js
@@ -2,8 +2,34 @@ import './style.css';
 import { ArrowRight, Plus } from '../../../svg';
 import { stories } from '../../../data/home';
 import Story from './Story';
+import {useMediaQuery} from 'react-responsive'
 
 const Storires = () => {
+
+   const query1175px = useMediaQuery({
+         query:"(max-width:1175px)",
+      });
+   const query1030px = useMediaQuery({
+      query:"(max-width:1030px)",
+   });
+   const query960px = useMediaQuery({
+      query:"(max-width:960px)",
+   });
+   const query885px = useMediaQuery({
+      query:"(max-width:885px)",
+   });
+
+   const max = 
+   query885px 
+   ? 5 
+   : query960px 
+   ? 4 
+   : query1030px 
+   ? 5 
+   : query1175px 
+   ? 4 
+   : stories.length;
+
    return (
       <div className="stories">
          <div className="create_story_card">
@@ -16,7 +42,7 @@ const Storires = () => {
             </div>
          </div>
          {
-            stories.map((story,i)=>(
+            stories.slice(0,max).map((story,i)=>(
                <Story story={story} key={i}/>
             ))
          }
