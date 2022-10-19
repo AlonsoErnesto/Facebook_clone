454a1f23639645d35cdc5dfb7765d9276a211755
3RN35T0#93 <46402355+AlonsoErnesto@users.noreply.github.com>
2022-10-18 20:19:28 -0500

    Delete facebook directory

--- facebook/backend/controllers/uplaod.js
+++
@@ -1,7 +0,0 @@
-exports.uploadImages = async (req, res) => {
-  try {
-    res.json("welcome from image upload");
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
